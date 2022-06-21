59b8dd961fe2fee9bdc05a1f0eec5afec3b7eb5f
dan-r95 <d.rossburg@googlemail.com>
2022-06-21 13:29:43 +0200

    MISC: add three js

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="about">
-    <h3>Hello, i am a coding challenge product.</h3>
-
+    <h3 class="row">Hello, i am a coding challenge product.</h3>
+    <Renderer ref="renderer" class="row">
+      <Camera :position="{ z: 200 }"/>
+      <Scene background="#4DBA87">
+        <AmbientLight color="#808080"/>
+        <PointLight :position="{ y: 50, z: 50 }"/>
+        <Text
+            ref="text"
+            text="Hello :)"
+            font-src="/src/assets/helv_regular.typeface.json"
+            align="center"
+            :size="30"
+            :height="5"
+            :position="{ x: 0, y: 0, z: 0 }"
+            :cast-shadow="true"
+        >
+          <PhongMaterial/>
+        </Text>
+      </Scene>
+    </Renderer>
     <div>
       <el-button
-        link
-        href="https://github.com/dan-r95/challenge"
-        @click="navigateToGit"
+          link
+          href="https://github.com/dan-r95/challenge"
+          @click="navigateToGit"
       >
         Visit my github profile
       </el-button>
@@ -15,15 +33,32 @@
 </template>
 
 <script lang="ts">
-import { defaultTo } from "lodash";
+import {Box, Camera, LambertMaterial, PointLight, Renderer, Scene, PhongMaterial, Text, AmbientLight} from 'troisjs';
+import { defineComponent, onMounted, ref } from 'vue';
+export default  defineComponent({
+  setup() {
+    const renderer = ref(null);
+    const text = ref(null);
+    onMounted(() => {
+      renderer?.value?.onBeforeRender(() => {
+        if( text.value.mesh ) {
+          text.value.mesh.rotation.x -= 0.03;
+        }
+      });
+    })
 
-export default {
+    return {
+      renderer,
+      text
+    }
+  },
+  components: {Box, Camera, LambertMaterial, PointLight, Renderer, Scene, PhongMaterial, Text, AmbientLight},
   methods: {
     navigateToGit: () => {
       window.open("https://github.com/dan-r95/challenge");
     },
   },
-};
+});
 </script>
 
 <style>
